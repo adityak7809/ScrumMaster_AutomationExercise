c0c040aa00fa558845a81726ced56fbc7695a791
adityak7809 <52339315+adityak7809@users.noreply.github.com>
2025-05-10 12:40:09 +0530

    Delete SauceDemo directory

--- SauceDemo/src/main/java/GenericRepo/FileLocations.java
+++
@@ -1,9 +0,0 @@
-package GenericRepo;
-
-public interface FileLocations {
-	
-	public String excelPath="./src/test/resources/TestData.xlsx";
-	public String propertyPath="./src/test/resources/CommonData.properties";
-	public long waitduration=30;
-
-}
